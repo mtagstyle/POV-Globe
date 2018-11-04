799674ba19dc9e6f89d6ea8bd962ba743ff7a609
mtagstyle <anthony.tran@live.ca>
2018-11-03 18:59:05 -0600

    Tried to optimize PPI interrupt sequence

--- firmware/sdk/v10/examples/peripheral/ppi/main.c
+++ firmware/sdk/v10/examples/peripheral/ppi/main.c
@@ -38,8 +38,8 @@
 #include "boards.h"
 #include "nordic_common.h"
 
-#define TIMER0_COMPARE_VALUE 6  // (0.35 us/(0.0625 us/tick)) == ~ 5.6  ticks)
-#define TIMER1_COMPARE_VALUE 14 // (0.9  us/(0.0625 us/tick)) == ~ 14.4 ticks)
+#define TIMER0_COMPARE_VALUE 5  // (0.35 us/(0.0625 us/tick)) == ~ 5.6  ticks)
+#define TIMER1_COMPARE_VALUE 15  // (0.9  us/(0.0625 us/tick)) == ~ 14.4 ticks)
 #define TIMER2_COMPARE_VALUE 20 // (1.25 us/(0.0625 us/tick)) ==   20 ticks  )
 
 /**
@@ -49,7 +49,7 @@
  * Callback args - None
  */
 nrf_drv_timer_config_t       timer_config = {NRF_TIMER_FREQ_16MHz, NRF_TIMER_MODE_TIMER, NRF_TIMER_BIT_WIDTH_32, 1, NULL};
-nrf_drv_gpiote_out_config_t  gpio_config  = {NRF_GPIOTE_POLARITY_HITOLO, NRF_GPIOTE_INITIAL_VALUE_LOW, true};
+nrf_drv_gpiote_out_config_t  gpio_config  = {NRF_GPIOTE_POLARITY_TOGGLE, NRF_GPIOTE_INITIAL_VALUE_LOW, true};
 
 const nrf_drv_timer_t timer0 = NRF_DRV_TIMER_INSTANCE(0);
 const nrf_drv_timer_t timer1 = NRF_DRV_TIMER_INSTANCE(1);
@@ -57,28 +57,62 @@ const nrf_drv_timer_t timer2 = NRF_DRV_TIMER_INSTANCE(2);
 
 #define GPIO_OUTPUT_PIN_NUMBER 12
 
-nrf_ppi_channel_t ppi_channel1, ppi_channel2;
+nrf_ppi_channel_t ppi_channel0, ppi_channel1, ppi_channel2, ppi_channel3, ppi_channel4, ppi_channel5, ppi_channel6;
 
-// Timer even handler. Not used since timer is used only for PPI.
+static uint8_t toggle = 0;
+/*
+void TIMER1_IRQHandler(void)
+{
+    
+    // Timer 0 Hook
+    if(toggle == 1)
+    {
+        NRF_PPI->CHENCLR = PPI_CHENCLR_CH0_Clear << ((uint32_t) ppi_channel1);
+        NRF_PPI->CHENSET = PPI_CHENSET_CH0_Set << ((uint32_t) ppi_channel0);
+        toggle = 0;
+    }
+    // Timer 1 Hook
+    else
+    {
+        NRF_PPI->CHENCLR = PPI_CHENCLR_CH0_Clear << ((uint32_t) ppi_channel0);
+        NRF_PPI->CHENSET = PPI_CHENSET_CH0_Set << ((uint32_t) ppi_channel1);
+        toggle = 1;
+    }
+}
+*/
 void timer_event_handler(nrf_timer_event_t event_type, void * p_context)
 {
-    nrf_drv_timer_clear(&timer0);
-    nrf_drv_timer_clear(&timer1);
-    nrf_drv_timer_clear(&timer2);
+    // Timer 0 Hook
+    if(toggle)
+    {
+        NRF_PPI->CHENCLR = PPI_CHENCLR_CH0_Clear << ((uint32_t) ppi_channel1);
+        NRF_PPI->CHENSET = PPI_CHENSET_CH0_Set << ((uint32_t) ppi_channel0);
+        /*
+        nrf_ppi_channel_disable(ppi_channel1);
+        nrf_ppi_channel_enable(ppi_channel0);
+        */
+    }
+    // Timer 1 Hook
+    else
+    {
+        NRF_PPI->CHENCLR = PPI_CHENCLR_CH0_Clear << ((uint32_t) ppi_channel0);
+        NRF_PPI->CHENSET = PPI_CHENSET_CH0_Set << ((uint32_t) ppi_channel1);
+        /*
+        nrf_ppi_channel_disable(ppi_channel0);
+        nrf_ppi_channel_enable(ppi_channel1);
+        */
+    }
 
-    // Turn GPIO on
-    nrf_drv_gpiote_out_task_force(GPIO_OUTPUT_PIN_NUMBER, 1);
-
-    // Reset all timers and turn them on
-    //nrf_drv_timer_resume(&timer0);
-    nrf_drv_timer_resume(&timer1);
-    nrf_drv_timer_resume(&timer2);
+    toggle = !toggle;
 }
 
 void start_shit()
 {
     // Turn GPIO on
     nrf_drv_gpiote_out_task_force(GPIO_OUTPUT_PIN_NUMBER, 1);
+
+    // Enable timer 1 hook first
+    nrf_drv_ppi_channel_enable(ppi_channel1);
 
     // Reset all timers and turn them on
     nrf_drv_timer_enable(&timer0);
@@ -109,11 +143,11 @@ static void init_timer()
     APP_ERROR_CHECK(error);
 
     // Timer 1, cutoff should be 0.9 us
-    nrf_drv_timer_extended_compare(&timer1, NRF_TIMER_CC_CHANNEL1, TIMER1_COMPARE_VALUE, NRF_TIMER_SHORT_COMPARE1_STOP_MASK, false);
+    nrf_drv_timer_extended_compare(&timer1, NRF_TIMER_CC_CHANNEL1, TIMER1_COMPARE_VALUE, NRF_TIMER_SHORT_COMPARE1_STOP_MASK, true);
     APP_ERROR_CHECK(error);
 
     // Timer 2, cutoff should be 1.25 us, interrupt here since it is the end of the sequence
-    nrf_drv_timer_extended_compare(&timer2, NRF_TIMER_CC_CHANNEL2, TIMER2_COMPARE_VALUE, NRF_TIMER_SHORT_COMPARE2_STOP_MASK, true);
+    nrf_drv_timer_extended_compare(&timer2, NRF_TIMER_CC_CHANNEL2, TIMER2_COMPARE_VALUE, NRF_TIMER_SHORT_COMPARE2_CLEAR_MASK, false);
     APP_ERROR_CHECK(error);
 }
 
@@ -128,19 +162,58 @@ static void init_ppi(void)
     err_code = nrf_drv_ppi_init();
     APP_ERROR_CHECK(err_code);
 
-    // Configure 1st available PPI channel to stop TIMER0 counter on TIMER1 COMPARE[0] match, which is every even number of seconds.
+    // Allocate our PPI channels
+    err_code = nrf_drv_ppi_channel_alloc(&ppi_channel0);
+    APP_ERROR_CHECK(err_code);
     err_code = nrf_drv_ppi_channel_alloc(&ppi_channel1);
     APP_ERROR_CHECK(err_code);
-    
-    // Connect the timer1 event to toggle the gpio (Should turn off)
-    err_code = nrf_drv_ppi_channel_assign(ppi_channel1, nrf_drv_timer_event_address_get(&timer1, NRF_TIMER_EVENT_COMPARE1), nrf_drv_gpiote_out_task_addr_get(GPIO_OUTPUT_PIN_NUMBER));
+    err_code = nrf_drv_ppi_channel_alloc(&ppi_channel2);
+    APP_ERROR_CHECK(err_code);
+    err_code = nrf_drv_ppi_channel_alloc(&ppi_channel3);
+    APP_ERROR_CHECK(err_code);
+    err_code = nrf_drv_ppi_channel_alloc(&ppi_channel4);
+    APP_ERROR_CHECK(err_code);
+    err_code = nrf_drv_ppi_channel_alloc(&ppi_channel5);
+    APP_ERROR_CHECK(err_code);
+    err_code = nrf_drv_ppi_channel_alloc(&ppi_channel6);
+    APP_ERROR_CHECK(err_code);
 
+    // Connect the timer0 event to toggle the gpio (Should turn OFF)
+    err_code = nrf_drv_ppi_channel_assign(ppi_channel0, nrf_drv_timer_event_address_get(&timer0, NRF_TIMER_EVENT_COMPARE0), nrf_drv_gpiote_out_task_addr_get(GPIO_OUTPUT_PIN_NUMBER));
+    APP_ERROR_CHECK(err_code);
+    // Connect the timer1 event to toggle the gpio (Should turn OFF)
+    err_code = nrf_drv_ppi_channel_assign(ppi_channel1, nrf_drv_timer_event_address_get(&timer1, NRF_TIMER_EVENT_COMPARE1), nrf_drv_gpiote_out_task_addr_get(GPIO_OUTPUT_PIN_NUMBER));
+    APP_ERROR_CHECK(err_code);
+    // Connect the timer2 event to clear timer0 task
+    err_code = nrf_drv_ppi_channel_assign(ppi_channel5, nrf_drv_timer_event_address_get(&timer2, NRF_TIMER_EVENT_COMPARE2), nrf_drv_timer_task_address_get(&timer0, NRF_TIMER_TASK_CLEAR));
+    APP_ERROR_CHECK(err_code);
+    // Connect the timer2 event to start timer0 task
+    err_code = nrf_drv_ppi_channel_assign(ppi_channel6, nrf_drv_timer_event_address_get(&timer2, NRF_TIMER_EVENT_COMPARE2), nrf_drv_timer_task_address_get(&timer0, NRF_TIMER_TASK_START));
+    APP_ERROR_CHECK(err_code);
+    // Connect the timer2 event to clear timer1 task
+    err_code = nrf_drv_ppi_channel_assign(ppi_channel2, nrf_drv_timer_event_address_get(&timer2, NRF_TIMER_EVENT_COMPARE2), nrf_drv_timer_task_address_get(&timer1, NRF_TIMER_TASK_CLEAR));
+    APP_ERROR_CHECK(err_code);
+    // Connect the timer2 event to start timer1 task
+    err_code = nrf_drv_ppi_channel_assign(ppi_channel3, nrf_drv_timer_event_address_get(&timer2, NRF_TIMER_EVENT_COMPARE2), nrf_drv_timer_task_address_get(&timer1, NRF_TIMER_TASK_START));
+    APP_ERROR_CHECK(err_code);
+    // Connect the timer2 event to toggle the gpio (Should turn ON)
+    err_code = nrf_drv_ppi_channel_assign(ppi_channel4, nrf_drv_timer_event_address_get(&timer2, NRF_TIMER_EVENT_COMPARE2), nrf_drv_gpiote_out_task_addr_get(GPIO_OUTPUT_PIN_NUMBER));
     APP_ERROR_CHECK(err_code);
 
     // Enable both configured PPI channel and GPIO Task
-    err_code = nrf_drv_ppi_channel_enable(ppi_channel1);
+    //err_code = nrf_drv_ppi_channel_enable(ppi_channel1);
+    //APP_ERROR_CHECK(err_code);
+    err_code = nrf_drv_ppi_channel_enable(ppi_channel2);
     APP_ERROR_CHECK(err_code);
-    
+    err_code = nrf_drv_ppi_channel_enable(ppi_channel3);
+    APP_ERROR_CHECK(err_code);
+    err_code = nrf_drv_ppi_channel_enable(ppi_channel4);
+    APP_ERROR_CHECK(err_code);
+    err_code = nrf_drv_ppi_channel_enable(ppi_channel5);
+    APP_ERROR_CHECK(err_code);
+    err_code = nrf_drv_ppi_channel_enable(ppi_channel6);
+    APP_ERROR_CHECK(err_code);
+
     nrf_drv_gpiote_out_task_enable(GPIO_OUTPUT_PIN_NUMBER);
 }
 
@@ -153,8 +226,6 @@ int main(void)
     init_gpio();
     init_ppi();
 
-    //nrf_timer_event_t event = NRF_TIMER_EVENT_COMPARE2;
-    //timer_event_handler(event, NULL);
     start_shit();
 
     // Loop and increment the timer count value and capture value into LEDs. @note counter is only incremented between TASK_START and TASK_STOP.
